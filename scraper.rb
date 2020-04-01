d15b1afb3b246d2a345b273c9a6e58e85b0c1beb
Mike Warren <mikeywarren89@gmail.com>
2020-04-01 16:25:48 +0900

    scrape image url stub complete

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 require 'nokogiri'
+
+results_hash = {}
 html_file = 'brewDog.html'
+
 doc = Nokogiri::HTML(open(html_file))
-p doc.search('.beer-item').first.img
+doc.search('.beer-item').each_with_index do |beer_item, i|
+  results_hash[i] = {
+    image: beer_item.search('img').attr('src').value.split("/")[2]
+  }
+end
+
+p results_hash
