bdbc4c7ddd2b71f046183b41a2198e2392f06005
Mike Warren <mikeywarren89@gmail.com>
2020-04-01 16:31:18 +0900

    added placeholder values for final hash

--- scraper.rb
+++ scraper.rb
@@ -8,6 +8,12 @@ html_file = 'brewDog.html'
 doc = Nokogiri::HTML(open(html_file))
 doc.search('.beer-item').each_with_index do |beer_item, i|
   results_hash[i] = {
+    name: 'TBD',
+    brewery: 'Brewdog',
+    abv: 'TBD',
+    desc: 'tbd',
+    ibu: 'tbd',
+    rating: '',
     image: beer_item.search('img').attr('src').value.split("/")[2]
   }
 end
