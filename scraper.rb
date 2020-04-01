e5d848d248236d96b3fd1cb121c41ae71720934b
Mike Warren <mikeywarren89@gmail.com>
2020-04-01 17:18:30 +0900

    rating scraper done

--- scraper.rb
+++ scraper.rb
@@ -14,7 +14,7 @@ doc.search('.beer-item').each_with_index do |beer_item, i|
     abv: beer_item.search('.abv').text.gsub(/\n/, '').strip,
     desc: beer_item.search('.desc')[1].text.gsub('Read Less', ''),
     ibu: beer_item.search('.ibu').text.gsub(/\n/, '').strip,
-    rating: '',
+    rating: beer_item.search('.num').text.gsub(/[()]/, ''),
     image: beer_item.search('img').attr('src').value.split('/')[2]
   }
 end
