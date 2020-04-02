84db6cd18823b87bba90e2f8654dac2aa12cab13
Mike Warren <mikeywarren89@gmail.com>
2020-04-02 11:14:00 +0900

    removed IBU from IBU number and got rid of extra whitespace on desc

--- scraper.rb
+++ scraper.rb
@@ -13,8 +13,8 @@ doc.search('.beer-item').each_with_index do |beer_item, i|
     brewery: 'Brewdog',
     style: beer_item.search('.style').text,
     abv: beer_item.search('.abv').text.gsub(/\n/, '').strip,
-    desc: beer_item.search('.desc')[1].text.gsub('Read Less', ''),
-    ibu: beer_item.search('.ibu').text.gsub(/\n/, '').strip,
+    desc: beer_item.search('.desc')[1].text.gsub('Read Less', '').strip,
+    ibu: beer_item.search('.ibu').text.gsub(/\n/, '').gsub('IBU', '').strip,
     rating: beer_item.search('.num').text.gsub(/[()]/, ''),
     image: beer_item.search('img').attr('src').value.split('/')[-1]
   }
